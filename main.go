e36e32a94b99190879f3cd5d28aaff82bb196ff5
Ivan Tsepov <spiritedbeaver@yandex.ru>
2023-08-04 17:28:52 +0300

    Several programs

--- main.go
+++ main.go
@@ -29,16 +29,34 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/julienschmidt/httprouter"
 )
+
+func variables(){
+	var a = "initial"
+	    fmt.Println(a)
+
+	    var b, c int = 1, 2
+	    fmt.Println(b, c)
+
+	    var d = true
+	    fmt.Println(d)
+
+	    var e int
+	    fmt.Println(e)
+
+	    f := "apple"
+	    fmt.Println(f)
+}
 
 func IndexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	w.WriteHeader(200)
 	w.Write([]byte("Работает"))
 }
 
-func main() {
+func web() {
 	log.Println("!i!i!i!")
 	//var cfg map[string]string
 	var cfg Config
@@ -49,5 +67,10 @@ func main() {
 
 	// log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", cfg["port"]), router))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), router))
+}
+
+func main() {
+	// web()
+	variables()
 
 }
